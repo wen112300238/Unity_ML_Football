454db4ac7909c1cd00ed5f10248d80361b8b9580
wen112300238 <55180649+wen112300238@users.noreply.github.com>
2020-04-30 16:15:23 +0800

    控制環境與事件開始

---
+++ Unity_ML_Footbal/Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    /// <summary>
+    /// 足球是否進入球門
+    /// </summary>
+    public static bool complete;
+
+    /// <summary>
+    /// 觸發開始事件 : 碰到勾選 Is Trigger 物件會執行一次
+    /// </summary>
+    /// <param name="other">碰到的物件資訊</param>
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //如果 碰到物件 的 名稱 等於 "進球感應區"
+        if (other.name == "進球感應區")
+        {
+            //進入球門
+            complete = true;
+        }
+
+    }
+
+}
